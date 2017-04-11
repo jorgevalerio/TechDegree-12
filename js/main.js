fcafec503a2ca3e76c0f58a76ae1405483a38d4c
jorgevalerio <jorgevalerio921@gmail.com>
2017-04-10 20:44:12 -0700

    First C

---
+++ js/main.js
@@ -0,0 +1,74 @@
+$('.sayhi').click(function() {
+  
+	$('#sayHi').modal({
+  
+	});
+	
+});
+
+//Event Listerners
+
+$('.portafolio1').click(function() {
+  
+	$('#portafolio1').modal({
+  
+	});
+	
+});
+
+$('.portafolio2').click(function() {
+  
+	$('#portafolio2').modal({
+  
+	});
+	
+});
+$('.portafolio3').click(function() {
+  
+	$('#portafolio3').modal({
+  
+	});
+	
+});
+$('.portafolio4').click(function() {
+  
+	$('#portafolio4').modal({
+  
+	});
+	
+});
+$('.portafolio5').click(function() {
+  
+	$('#portafolio5').modal({
+  
+	});
+	
+});
+$('.portafolio6').click(function() {
+  
+	$('#portafolio6').modal({
+  
+	});
+	
+});
+$('.portafolio7').click(function() {
+  
+	$('#portafolio7').modal({
+  
+	});
+	
+});
+$('.portafolio8').click(function() {
+  
+	$('#portafolio8').modal({
+  
+	});
+	
+});
+$('.portafolio9').click(function() {
+  
+	$('#portafolio9').modal({
+  
+	});
+	
+});
